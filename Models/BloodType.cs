281809761a60b893f746a63bece2586a079d33e6
delta663 <dekdel@gmail.com>
2026-04-13 18:59:38 +0700

    Create BloodType.cs

---
+++ Models/BloodType.cs
@@ -0,0 +1,13 @@
+namespace PrisonerBlood.Models;
+public enum BloodType
+{
+    Creature = 524822543,
+    Warrior = -516976528,
+    Rogue = -1620185637,
+    Brute = 804798592,
+    Scholar = 1476452791,
+    Worker = -1776904174,
+    Mutant = 1821108694,
+    Draculin = 1328126535,
+    Corrupted = -1382693416,
+}
