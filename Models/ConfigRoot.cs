23d55ac1e7e0f854cca21efad60f1b93aabc533e
delta663 <dekdel@gmail.com>
2026-04-24 21:30:05 +0700

    Update ConfigRoot.cs

--- Models/ConfigRoot.cs
+++ Models/ConfigRoot.cs
@@ -1,7 +1,12 @@
 namespace PrisonerBlood.Models;
 
-internal sealed class ConfigRoot
+internal sealed class BuyConfigRoot
 {
-    public ConfigSection Prisoner { get; set; } = new();
-    public ConfigSection BloodPotion { get; set; } = new();
+    public BuySection Prisoner { get; set; } = new();
+    public BuySection BloodPotion { get; set; } = new();
+}
+
+internal sealed class SellConfigRoot
+{
+    public SellSection Prisoner { get; set; } = new();
 }
