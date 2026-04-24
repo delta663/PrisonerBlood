a46d2aadac7d00edb4e7eddb35f74c8c5727572c
delta663 <dekdel@gmail.com>
2026-04-24 21:30:26 +0700

    Update ConfigSection.cs

--- Models/ConfigSection.cs
+++ Models/ConfigSection.cs
@@ -2,11 +2,21 @@ using System.Collections.Generic;
 
 namespace PrisonerBlood.Models;
 
-internal sealed class ConfigSection
+internal sealed class BuySection
 {
     public bool Enabled { get; set; } = true;
     public int CurrencyPrefab { get; set; }
     public string CurrencyName { get; set; } = string.Empty;
     public int DefaultCost { get; set; }
     public Dictionary<string, int> BloodCosts { get; set; } = new();
+}
+
+internal sealed class SellSection
+{
+    public bool Enabled { get; set; } = true;
+    public float MinSellableQuality { get; set; }
+    public int CurrencyPrefab { get; set; }
+    public string CurrencyName { get; set; } = string.Empty;
+    public int DefaultPrice { get; set; }
+    public Dictionary<string, int> BloodPrices { get; set; } = new();
 }
